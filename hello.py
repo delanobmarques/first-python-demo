6cb06c03fb97dbd1297bbf7178a9f3a02edf1c3c
Delano Marques <54899930+delanobmarques@users.noreply.github.com>
2023-09-01 10:10:30 -0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Author: Delano Marques
+# Date: September 6, 2023
+# Description: Simple Hello World App
+
+def main():
+    # create a variable for the user's name
+    name = "Delano"
+
+    # output message to the user
+    print("Hello, " + name + "!")
+
+if __name__ == "__main__":
+    main()
